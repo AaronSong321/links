9304f7e5ca2fb6b573886e63519dee51b2ec5d5c
AaronSong321 <2511146542@qq.com>
2021-06-20 22:23:45 +0100

    local dev

--- tests/wasm/runwasmtest.sh
+++ tests/wasm/runwasmtest.sh
@@ -1,6 +1,6 @@
 # make && ./links -d --set=show_compiled_ir=true ./tests/wasm/b.links && wat2wasm ./tests/wasm/a.wat -o ./tests/wasm/a.wasm
 showCompiledIr="--set=show_compiled_ir=true"
-# showCompiledIr=""
+showCompiledIr=""
 if make; then
 	for linksFile in $(find ./tests/wasm -name "[ab]*.links" -print); do
 		filenameNoExt=${linksFile:0:${#linksFile}-6};
